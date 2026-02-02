2c85ec7bbbb1d2336c5f0373eac8731c7a8c643e
JUGALSUTHAR29 <lalitsuthar9130@gmail.com>
2026-02-02 11:37:51 +0530

    Add files via upload

--- one.js
+++ one.js
@@ -1,32 +1,5 @@
-let todo =[];
-let req = prompt ("enter your request");
-while(true) {
-        if(req == "quit"){
-                console.log("quiting the app");
-                break;
-        }
-        if(req == "list") {
-                console.log("---------");
-                for(let i=0; i<todo.length; i++) {
-                        console.log(i, todo[i]);
-                }
-                console.log("-----------");
-        }
-        else if (req == "add") {
-                let task = prompt("please enter your task whhich you want to be add")
-                todo.push(task);
-                console.log("task added");
-        }
-        else if (req == "delete") {
-                let task = prompt("please enter your task which you want to be delete")
-                todo.splice(idx , 1);
-                console.log("task deleted");        
-        }
-        else {
-                console.log("wrong request");
-        }
-        req = prompt("please enter your request") 
-}
-
-
-
+gsap.to(#box, {
+        x:1200,
+        rotate:360,
+        
+})
