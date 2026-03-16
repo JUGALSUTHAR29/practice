3bdb129bcee05f162baf57c41effe10e07aa182a
JUGALSUTHAR29 <lalitsuthar9130@gmail.com>
2026-03-16 10:29:47 +0530

    Add files via upload

--- four.js
+++ four.js
@@ -1,29 +1,3 @@
-function connecttoserver(){
-        console.log("conecting to server...");
-        return new Promise(function(resolve, reject){
-                setTimeout(function(){
-                        console.log("connected...");
-                },2000)
-        })
-}
-
-connecttoserver()
-.then()
-.catch()
-function getcourses(){
-        console.log("geting courses...");
-
-return new Promise(function(resolve, reject){
-        setTimeout(function(){
-                resolve(["course1","course2","course3"])
-        },2000)
-  })
-}
-connecttoserver()
-.then(function(response){
-        console.log(response);
-        return getcourses()
-})
-.then(function(response){
-        console.log(response);
-})
+a = 10
+b = 20
+console.log(a+b)
--- one.js
+++ one.js
@@ -1,5 +1,3 @@
-gsap.to(#box, {
-        x:1200,
-        rotate:360,
-        
-})
+ver a=20;
+ver b= 25
+ver(a+b):
